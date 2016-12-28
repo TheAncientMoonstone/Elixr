363002e0ab9e1715997b7e4498ffd2c86f7980bc
Timothy Richardson <timothyrichardson@Timothy-Richardsons-MacBook-Pro.local>
2016-12-28 23:24:42 +1100

    Replaced IGListKit with ASyncDisplayKit and adjusting supporting files to ensure a smooth transition.

--- Podfile
+++ Podfile
@@ -5,5 +5,5 @@ xcodeproj 'Elixr'
 target :Elixr
 
 	source 'https://github.com/CocoaPods/Specs.git'
-		pod 'IGListKit', '~> 2.0.0'
+		pod 'AsyncDisplayKit'
 	use_frameworks!	
--- Pods/Target Support Files/Pods-Elixr/Pods-Elixr-frameworks.sh
+++ Pods/Target Support Files/Pods-Elixr/Pods-Elixr-frameworks.sh
@@ -89,10 +89,14 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/IGListKit/IGListKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/AsyncDisplayKit/AsyncDisplayKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PINCache/PINCache.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PINRemoteImage/PINRemoteImage.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/IGListKit/IGListKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/AsyncDisplayKit/AsyncDisplayKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PINCache/PINCache.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PINRemoteImage/PINRemoteImage.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
