b69f180e22f05a26b45ebb34b50b8c3cb26da691
TheAncientMoonstone <timothy.richardson01@gmail.com>
2017-02-04 16:34:38 +1100

    Updated AWSConfiguration class

--- Elixr/AWSConfiguration.swift
+++ Elixr/AWSConfiguration.swift
@@ -11,14 +11,14 @@ import AWSCore
 
 
 // Cognito User Pools Identity Id
-let AWSCognitoUserPoolId: String = "ap-northeast-1_DR3tTJqrD"
+let AWSCognitoUserPoolId: String = "ap-northeast-1_0pohJtGIA"
 
 // Cognito User Pools App Client Id
-let AWSCognitoUserPoolAppClientId: String = "5pmvgst5c9650q4v9dfii8g8h7"
+let AWSCognitoUserPoolAppClientId: String = "7rejk2lmqt5tb7lugqjn389jiq"
 
 // Cognito User Pools Region
 let AWSCognitoUserPoolRegion: AWSRegionType = .apNortheast1
 
 // Cognito User Pools Client Secret
-let AWSCognitoUserPoolClientSecret: String = "u6tnc60d0g48cfu4ro0pnfmn7hh32pfu3k8hqdseb7dl72iebr6"
+let AWSCognitoUserPoolClientSecret: String = "1ohtlk00p6h98til7g31odks7ev9jdb86s8bbbfdpq450i68g4gt"
 
