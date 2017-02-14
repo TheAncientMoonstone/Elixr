e144baaf6ad20c2dbec80f6eb1a25db0ea0a447f
TheAncientMoonstone <timothy.richardson01@gmail.com>
2017-02-15 00:30:32 +1100

    Added code for logout

--- Elixr/MainViewController.swift
+++ Elixr/MainViewController.swift
@@ -25,6 +25,41 @@ class MainViewController: UIViewController {
         
         // Present the Sign In View Controller.
         presentSignInViewController()
+        
+        signInObserver = NotificationCenter.default.addObserver(forName: NSNotification.Name.AWSIdentityManagerDidSignIn, object: AWSIdentityManager.defaultIdentityManager(), queue: OperationQueue.main, using: {[weak self] (note: Notification) -> Void in
+            guard let strongSelf = self else { return }
+            print("Sign in Observer observed the successful sign in")
+            strongSelf.setupRightBarButtonItem()
+        })
+        
+        signOutObserver = NotificationCenter.default.addObserver(forName: NSNotification.Name.AWSIdentityManagerDidSignOut, object: AWSIdentityManager.defaultIdentityManager(), queue: OperationQueue.main, using: {[weak self](note: Notification) -> Void in
+            guard let strongSelf = self else { return }
+            print("Sign out observer observed the successful sign out")
+            strongSelf.setupRightBarButtonItem()
+        })
+        
+        setupRightBarButtonItem()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(signInObserver)
+        NotificationCenter.default.removeObserver(signOutObserver)
+    }
+    
+    func setupRightBarButtonItem() {
+        struct Static {
+            static let onceToken: dispatch_once_t = 0
+        }
+        
+        dispatch_once(&Static.onceToken, {
+            let loginButton: UIBarButtonItem = UIBarButtonItem(title: nil, style: .Done, target: self, action: nil)
+            self.navigationItem.rightBarButtonItem = loginButton
+        })
+        
+        if (AWSIdentityManager.defaultIdentityManager().isLoggedIn) {
+            navigationItem.rightBarButtonItem!.title = NSLocalizedString("Sign-Out", comment: "Label for the logout button.")
+            navigationItem.rightBarButtonItem!.action = #selector(MainViewController.handleLogout)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,10 +70,9 @@ class MainViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        // Present the Sign In View Controller.
-        // presentSignInViewController()
-        
     }
+
+    
 
     // MARK:- Sign In View Controller.
     // Present the Sign In View Controller.
