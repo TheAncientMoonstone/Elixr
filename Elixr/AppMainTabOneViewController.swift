308e5a123be35a6271f470f49a3292c37e50940d
TheAncientMoonstone <timothy.richardson01@gmail.com>
2017-02-15 16:20:40 +1100

    Incremental commit

--- Elixr/AppMainTabOneViewController.swift
+++ Elixr/AppMainTabOneViewController.swift
@@ -12,11 +12,12 @@ import AWSCognito
 import AWSCognitoIdentityProvider
 
 class AppMainTabOneViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
