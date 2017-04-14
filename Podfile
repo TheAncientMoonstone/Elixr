daba5ab13a0ce12f8ec24bce71acded46d1c41ba
TheAncientMoonstone <timothy.richardson01@gmail.com>
2017-04-14 22:52:31 +1000

    Inclusion of Socket-IO-Swift library to the project.

--- Podfile
+++ Podfile
@@ -7,4 +7,5 @@ target :Elixr
 	source 'https://github.com/CocoaPods/Specs.git'
 			pod 'JSQMessagesViewController'
 			pod 'Alamofire'
+			pod 'Socket.IO-Client-Swift'
 	use_frameworks!
--- Pods/Target Support Files/Pods-Elixr/Pods-Elixr-frameworks.sh
+++ Pods/Target Support Files/Pods-Elixr/Pods-Elixr-frameworks.sh
@@ -92,11 +92,13 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JSQMessagesViewController/JSQMessagesViewController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JSQSystemSoundPlayer/JSQSystemSoundPlayer.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Socket.IO-Client-Swift/SocketIO.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JSQMessagesViewController/JSQMessagesViewController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JSQSystemSoundPlayer/JSQSystemSoundPlayer.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Socket.IO-Client-Swift/SocketIO.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
