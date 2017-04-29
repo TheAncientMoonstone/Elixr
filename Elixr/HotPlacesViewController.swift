5521c3ad35358179414dbac3acc9e726101a9244
TheAncientMoonstone <timothy.richardson01@gmail.com>
2017-04-29 20:48:33 +1000

    Updated CoocaPods and added further market functionality to the project.

--- Elixr/HotPlacesViewController.swift
+++ Elixr/HotPlacesViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class HotPlacesViewController: UIViewController {
 
@@ -21,15 +23,4 @@ class HotPlacesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
