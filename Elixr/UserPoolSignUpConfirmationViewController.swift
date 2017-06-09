902d722ff4677dac1903c50db0fd2c29ac0237b5
TheAncientMoonstone <timothy.richardson01@gmail.com>
2017-06-09 15:35:29 +1000

    Overhaul of CocoaPods installed in this project.

--- Elixr/UserPoolSignUpConfirmationViewController.swift
+++ Elixr/UserPoolSignUpConfirmationViewController.swift
@@ -21,7 +21,6 @@ class UserPoolSignUpConfirmationViewController: UIViewController {
     @IBOutlet weak var Username: UITextField!
     @IBOutlet weak var confirmationCode: UITextField!
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -50,11 +49,11 @@ class UserPoolSignUpConfirmationViewController: UIViewController {
         
         // Custom Color
         pastelView.setColors([#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1),
-                                      #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1),
-                                      #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1),
-                                      #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1),
-                                      #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),
-                                      #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)])
+                              #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1),
+                              #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1),
+                              #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1),
+                              #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),
+                              #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)])
         
         pastelView.startAnimation()
         view.insertSubview(pastelView, at: 0)
@@ -88,7 +87,6 @@ class UserPoolSignUpConfirmationViewController: UIViewController {
         })
     
     }
-    
     
     @IBAction func ResendConfirmationCode(_ sender: Any) {
         self.user?.resendConfirmationCode().continueWith (block: {[weak self] (task: AWSTask) -> AnyObject? in
